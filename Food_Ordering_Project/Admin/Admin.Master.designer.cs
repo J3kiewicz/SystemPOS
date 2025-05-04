97110d740dc57341be8a26be34422ed728af1d79
Julia <trzeciakiewicz.julia@gmail.com>
2025-05-04 18:59:01 +0200

    Dodaj pliki projektów.

---
+++ Food_Ordering_Project/Admin/Admin.Master.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <generowane automatycznie>
+//     Ten kod został wygenerowany przez narzędzie.
+//
+//     Zmiany w tym pliku mogą spowodować niewłaściwe zachowanie i zostaną utracone
+//     w przypadku ponownego wygenerowania kodu. 
+// </generowane automatycznie>
+//------------------------------------------------------------------------------
+
+namespace Food_Ordering_Project.Admin
+{
+
+
+    public partial class Admin
+    {
+
+        /// <summary>
+        /// Kontrolka head.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+
+        /// <summary>
+        /// Kontrolka form1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Kontrolka lbLogout.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lbLogout;
+
+        /// <summary>
+        /// Kontrolka ContentPlaceHolder1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+    }
+}
---
+++ Food_Ordering_Project/User/User.Master.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <generowane automatycznie>
+//     Ten kod został wygenerowany przez narzędzie.
+//
+//     Zmiany w tym pliku mogą spowodować niewłaściwe zachowanie i zostaną utracone
+//     w przypadku ponownego wygenerowania kodu. 
+// </generowane automatycznie>
+//------------------------------------------------------------------------------
+
+namespace Food_Ordering_Project.User
+{
+
+
+    public partial class User
+    {
+
+        /// <summary>
+        /// Kontrolka head.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+
+        /// <summary>
+        /// Kontrolka form1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Kontrolka lbRegisterOrProfile.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lbRegisterOrProfile;
+
+        /// <summary>
+        /// Kontrolka lbLoginOrLogout.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton lbLoginOrLogout;
+
+        /// <summary>
+        /// Kontrolka pnlSliderUC.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlSliderUC;
+
+        /// <summary>
+        /// Kontrolka ContentPlaceHolder1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// Aby wprowadzić zmiany, przenieś deklarację pola z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+    }
+}
